bd4ecc7507261ef67553b5a33350e2e0d4dfcce6
shtwangy <shtwangy@gmail.com>
2020-11-05 22:01:36 +0900

    feat(src/AdmissionFeeCalculator.tsx): pass detail props to Detail component.

--- src/AdmissionFeeCalculator.tsx
+++ src/AdmissionFeeCalculator.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import Summary from "./Summary";
 import Detail from "./Detail";
+import {DetailProps} from "./types/props/DetailProps";
 
 class AdmissionFeeCalculator extends React.Component {
+    private detail: DetailProps = {
+        classification: {
+            name: "大人",
+            description: "",
+            unitPrice: 1000,
+            numOfPeople: 0,
+            totalPrice: 0
+        }
+    };
     render() {
         return (
             <>
-                <Detail />
+                <Detail classification={this.detail.classification}/>
                 <Summary />
             </>
         );
